ed42d3462a9eb9a63261578de993b2b90e753128
Pecherle George <97780041+Pechi23@users.noreply.github.com>
2022-10-29 20:56:04 +0300

    the set 2 is finished

--- Set2/Program.cs
+++ Set2/Program.cs
@@ -274,41 +274,9 @@ namespace Set2
             Console.WriteLine("Sa se determine daca o secventa de n numere este monotona. Secventa monotona = secventa monoton crescatoare sau monoton descrescatoare.");
             var line = ReadInputs();
 
-            bool isSorted = true;
-            bool isGrowing = true;
+            bool isSorted = true, isGrowing = true;
+            monoton(line, ref isSorted, ref isGrowing);        
 
-            int ant = line[0];
-            foreach (int e in line)
-            {
-                if (e < ant)
-                {
-                    isGrowing = false;
-                    break;
-                }
-                ant = e;
-            }
-
-            foreach (int e in line)
-            {
-                if (isGrowing)
-                {
-                    if (e < ant)
-                    {
-                        isSorted = false;
-                        break;
-                    }
-                    ant = e;
-                }
-                else
-                {
-                    if (e > ant)
-                    {
-                        isSorted = false;
-                        break;
-                    }
-                    ant = e;
-                }
-            }
             if (isSorted)
             {
                 if (isGrowing)
@@ -318,6 +286,31 @@ namespace Set2
             }
             else
                 Console.WriteLine("Sirul nu este monoton");
+        }
+        /// <summary>
+        /// this method analize a sequence and modify the last to parameters based on the sequence properties
+        /// </summary>
+        /// <param name="arr"></param>
+        static void monoton(int [] arr, ref bool isSorted, ref bool isGrowing)
+        {
+            isSorted = true;
+            isGrowing = true;
+
+            for(int i=1;i<arr.Length && isGrowing;i++)
+                if (arr[i-1] > arr[i])
+                    isGrowing = false;
+
+            for (int i = 1; i < arr.Length && isSorted; i++)
+                if (isGrowing)
+                {
+                    if (arr[i - 1] > arr[i])
+                        isSorted = false;
+                }
+                else
+                {
+                    if (arr[i - 1] < arr[i])
+                        isSorted = false;
+                }
         }
 
         /// <summary>
@@ -403,32 +396,24 @@ namespace Set2
         /// </summary>
         static void P13()
         {
-            //TODO: bool method returning the result
             Console.WriteLine("O .secventa crescatoare rotita. este o secventa de numere care este in ordine crescatoare sau poate fi transformata intr-o secventa in ordine crescatoare prin rotiri succesive \n(rotire cu o pozitie spre stanga = toate elementele se muta cu o pozitie spre stanga si primul element devine ultimul). \nDeterminati daca o secventa de n numere este o secventa crescatoare rotita.");
             var line = ReadInputs();
 
-            bool isOk = true;
-            int minSecv1 = line[0];
-
-            int ant = line[0];
-            int i = 0;
-            while (line[i] >= ant && i < line.Length - 1)
-                ant = line[i++];
-
-
-            while (i < line.Length && isOk)
+            for (int i = 0; i < line.Length - 1; i++)
             {
-                if (line[i] < ant)
-                    isOk = false;
-                if (i == line.Length - 2 && line[i] > minSecv1)
-                    isOk = false;
-                ant = line[i++];
-
+                bool isSorted = true, isGrowing = true;
+                monoton(line, ref isSorted, ref isGrowing);
+                if (isSorted)
+                {
+                    if (isGrowing)
+                    {
+                        Console.WriteLine("Secventa este monotona rotita (crescatoare)");
+                        return;
+                    }
+                }
+                rotateToLeft(ref line);
             }
-            if(isOk)
-                Console.WriteLine("Secventa este crescatoare rotita");
-            else
-                Console.WriteLine("Secventa nu este crescatoare rotita");
+            Console.WriteLine("Secventa nu este crescatoare rotita");
         }
 
         /// <summary>
@@ -436,37 +421,38 @@ namespace Set2
         /// </summary>
         static void P14()
         {
-
-            //TODO: simulez n-1 rotiri si pt fiecare rotire incerc sa vad daca secventa poate fi monotona, folosindu-ma de rezolvarea de la problema anterioara
+            //simulez n-1 rotiri si pt fiecare rotire incerc sa vad daca secventa poate fi monotona, folosindu-ma de rezolvarea de la problema 9
             Console.WriteLine("O secventa monotona rotita este o secventa de numere monotona sau poate fi transformata intr-o secventa montona prin rotiri succesive. Determinati daca o secventa de n numere este o secventa monotona rotita.");
             var line = ReadInputs();
 
-            /*
-            //we try to create 2 sorted lists
-            List<int> list1 = new List<int>();
-            List<int> list2 = new List<int>();
-
-            int indexOfMax = Array.IndexOf(line,line.Max());
-            for(int i=0;i<indexOfMax;i++)
+            for (int i = 0; i < line.Length-1; i++)
             {
-                
-            }
-
-            if ()//if the sequence is sorted
-            {
-                Console.WriteLine("Secventa nu este bitonica");
-                return;
-            }
-
-            for (int i = 1; i < listaDescresc.Count; i++)
-            {
-                if (listaDescresc[i - 1] < listaDescresc[i])//if there is another local max the
+                bool isSorted = true, isGrowing = true;
+                monoton(line, ref isSorted, ref isGrowing);
+                if(isSorted)
                 {
-                    Console.WriteLine("Secventa nu este bitonica");
+                    if(isGrowing)
+                        Console.WriteLine("Secventa este monotona rotita (crescatoare)");
+                    else
+                        Console.WriteLine("Secventa este monotona rotita (descescatoare)");
                     return;
                 }
+                rotateToLeft(ref line);
             }
-            */
+
+            Console.WriteLine("Secventa nu este monotona rotita");
+        }
+
+        /// <summary>
+        /// rotate the actual array to left with one position (the leftmost element become the last after the rotation)
+        /// </summary>
+        /// <param name="arr"></param>
+        public static void rotateToLeft(ref int[] arr)
+        {
+            int temp = arr[0];
+            for (int i = 0; i < arr.Length - 1; i++)
+                arr[i] = arr[i + 1];
+            arr[arr.Length - 1] = temp;
         }
 
         /// <summary>
@@ -474,41 +460,35 @@ namespace Set2
         /// </summary>
         static void P15()
         {
-            //TODO: bool method returning the result
             Console.WriteLine("O secventa bitonica este o secventa de numere care incepe monoton crescator si continua monoton descrecator. De ex. 1,2,2,3,5,4,4,3 este o secventa bitonica. Se da o secventa de n numere. Sa se determine daca este bitonica. ");
             var line = ReadInputs();
 
-            //we try to create 2 sorted lists where the second should be in descending order to have the required property
-            List<int> listaCresc = new List<int>();
-            List<int> listaDescresc = new List<int>();
-
-            bool insertInListCresc = true;
-            listaCresc.Add(line[0]);
-            for (int i = 1; i < line.Length; i++)
-            {
-                if (line[i] < line[i - 1])
-                    insertInListCresc = false;
-                if (insertInListCresc)
-                    listaCresc.Add(line[i]);
-                else
-                    listaDescresc.Add(line[i]);
-            }
-
-            if ((listaCresc.Count == 1 && line[0] > line[1]) || listaDescresc.Count == 0)//if the sequence is sorted
-            {
+            if (isBitonic(line))
+                Console.WriteLine("Secventa este bitonica");
+            else
                 Console.WriteLine("Secventa nu este bitonica");
-                return;
-            }
+        }
+        /// <summary>
+        /// returns true if the given array is bitonic and false otherwise
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public static bool isBitonic(int [] arr)
+        {
+            int posMax = Array.IndexOf(arr,arr.Max());
+            
+            if(posMax==0 || posMax==arr.Length)
+                return false;
 
-            for (int i = 1; i < listaDescresc.Count; i++)
-            {
-                if (listaDescresc[i - 1] < listaDescresc[i])//if there is another local max the
-                {
-                    Console.WriteLine("Secventa nu este bitonica");
-                    return;
-                }
-            }
-            Console.WriteLine("Secventa este bitonica");//otherwise the sequence has the required property
+            for (int i = 0; i < posMax; i++)
+                if (arr[i] > arr[i + 1])
+                    return false;
+
+            for (int i = posMax; i < arr.Length-1; i++)
+                if (arr[i] < arr[i + 1])
+                    return false;
+
+            return true;
         }
 
         /// <summary>
@@ -516,7 +496,22 @@ namespace Set2
         /// </summary>
         static void P16()
         {
-            //TODO: simulez n-1 rotiri si in fiecare stare incerc sa vad daca poate fi secventa bitonica, folosindu-ma de rezolvarea de la problema anterioara
+            Console.WriteLine("O .secventa bitonica rotita. este o secventa bitonica sau una ca poate fi transformata intr-o secventa bitonica prin rotiri succesive (rotire = primul element devine ultimul). Se da o secventa de n numere. Se cere sa se determine daca este o secventa bitonica rotita. ");
+            var line = ReadInputs();
+
+            bool sol = false;
+            for(int i=0;i<line.Length-1 && !sol;i++)
+            {
+                if (isBitonic(line))
+                    sol = true;
+                rotateToLeft(ref line);
+            }
+
+            if (sol)
+                Console.WriteLine("Secventa este bitonica rotita");
+            else
+                Console.WriteLine("Secventa nu este bitonica rotita");
+
         }
 
         /// <summary>

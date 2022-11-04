d26a33ea25a925dedb3ba55e97079c9cfca8ac6d
Pecherle George <97780041+Pechi23@users.noreply.github.com>
2022-11-04 14:57:30 +0200

    finished

---
+++ BinaryClock/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        private static void Main()
+        {
+            String data = DateTime.Now.ToString("HH:mm:ss");
+            //Console.WriteLine($"{data}");
+            String[] line = data.Split(':');
+            Console.WriteLine($"H:{base10ToBaseK(2, line[0][0])}");
+            Console.WriteLine($"H:{base10ToBaseK(2, line[0][1])}");
+            Console.WriteLine($"M:{base10ToBaseK(2, line[1][0])}");
+            Console.WriteLine($"M:{base10ToBaseK(2, line[1][1])}");
+            Console.WriteLine($"S:{base10ToBaseK(2, line[2][0])}");
+            Console.WriteLine($"S:{base10ToBaseK(2, line[2][1])}");
+        }
+
+        /// <summary>
+        /// converts an input from base 10 to base b1(b1<=10) and returns the result
+        /// </summary>
+        /// <param name="b1"></param>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private static string base10ToBaseK(int b1, int input)
+        {
+            string sol = "";
+            Stack<char> stack = new Stack<char>();
+            while (input > 0)
+            {
+                stack.Push((char)('0' + input % b1));
+                input /= b1;
+            }
+            foreach (char c in stack)
+                sol += c;
+            return sol;
+        }
+    }
+}
